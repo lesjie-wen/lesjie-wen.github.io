506ec708802f9b0a6d45c0458229c7b65fba3b2c
lesjie-wen <870351470@qq.com>
2024-10-06 19:25:33 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
 import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
+
+let assetPrefix = `/${repo}/`
+
+// 用于为应用设置基础路径
+// 这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
+let basePath = `/${repo}`
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   output: 'export',
-  //assetPrefix: './', //'/lesjie-wen.github.io/',
-  //basePath: './', //'/lesjie-wen.github.io',
+  assetPrefix: assetPrefix, //'/lesjie-wen.github.io/',
+  basePath: basePath, //'/lesjie-wen.github.io',
   images: {
     unoptimized: true,
   },
